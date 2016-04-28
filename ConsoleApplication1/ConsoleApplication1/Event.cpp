e7d81967782a356583ecc5c68a1c144d4bdce4b7
Nate <thenekkbreaker@gmail.com>
2016-04-27 21:55:55 -0400

    "Finished" event.cpp

--- ConsoleApplication1/ConsoleApplication1/Event.cpp
+++ ConsoleApplication1/ConsoleApplication1/Event.cpp
@@ -1,2 +1,28 @@
 #include "Event.h"
 
+using namespace std;
+
+Event::Event(int t, int wN, WireState s, int c) {
+	time = t;
+	wireN = wN;
+	state = s;
+	count = c;
+}
+
+int Event::getWireNumber() {
+	return wireN;
+}
+
+WireState Event::getWireState() {
+	return state;
+}
+
+bool Event::operator<(const Event e) const {
+	if (time < e.time) {
+		return true;
+	}
+	else if (time == e.time && count < e.count) {
+		return true;
+	}
+	else return false;
+}
--- ConsoleApplication1/ConsoleApplication1/Event.h
+++ ConsoleApplication1/ConsoleApplication1/Event.h
@@ -1,13 +1,16 @@
 #include "Enumerations.h"
 class Event{
+	public:
+		Event(int t, int wN, WireState s, int c);
+
+		int getWireNumber();
+		WireState getWireState();
+
+		bool operator<(const Event e) const;
 
 	private:
 		int time;
 		int wireN;
 		WireState state;
 		int count;
-
-	// wire pointer
-	//wireState
-	//count or arrival order, creation number
 };
